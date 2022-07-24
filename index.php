27adb5b04fdc6d663ba85a682e4f5aedfbfefc18
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 17:15:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,26 +13,24 @@
    }
 
 //Mostrar os livros
-   if (isset($_POST['mostrar']) && isset($_POST['LivroEspecifico'])){
+   if (isset($_POST['mostrar'])){
 
-      //header( #ADICIONAR CAMINHO PARA O MOSTRAR ESPECÍFICO);
+      //header( #ADICIONAR CAMINHO PARA O MOSTRAR);
 
    }
-
 //Mostrar um livro específico
-   if (isset($_POST['mostrar']) && isset($_POST['LivroEspecifico'])){
+   if (isset($_POST['MostrarTudo'])){
       $_SESSION['LivroEspecifico'] = $_POST['LivroEspecifico'];
       //header( #ADICIONAR CAMINHO PARA O MOSTRAR ESPECÍFICO);
-}
-
+   }
 ?>
 
+<!DOCTYPE html>
 <html>
     <head>
         <title>pagina de requisição GET e POST</title>
     </head>
     <body>
-        Campos a serem preenchidos
         <form method="post">
             <input type="text" name="nomeAutor" placeholder="Nome autor">
             <br>
@@ -53,20 +51,18 @@
             <input type="text" name="assuntos" placeholder="Assuntos abordados no livro">
             <br>
             <input type="submit" value="Inserir Livro">
-    </form>
-    <form method="post">
-            Preencher para mostrar 
-            <br>
-            Livro específico:(código) 
-            <br>
-            <input type="text" name="LivroEspecifico">
-            <br><br>
+        </form>
+        <form method="post">
+            Preencher para mostrar <br>
+            Livro específico:(código) <br>
+            <input type="text" name="LivroEspecifico"><br><br>
             <input type="submit" name="mostrar" value="Mostrar Livros">
-     </form>
-     <br>
-     <form method="post">
-        <input type="submit" name="limpar" value="limpar cache">
-     </form>
-     <br>
+            <br><br>
+            <input type="submit" name="MostrarTudo" value="Mostrar tudo">
+         </form>
+         <br>
+         <form method="post">
+            <input type="submit" name="limpar" value="limpar cache">
+         </form><br>
     </body>
 </html>
