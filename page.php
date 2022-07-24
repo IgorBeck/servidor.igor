208fc384d7925288800dca413bfeac51ca869802
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 20:09:57 -0300

    Update page.php

--- page.php
+++ page.php
@@ -16,10 +16,9 @@
 		foreach ($dadosJsonDecodificados as $key => $value) {
     			if($value == $isbn) {
         			$msg = 'Encontrado na lista!';
-				print_r($msg);
-				print_r($value);
-				
+				print_r($msg);	
 			}
+			print_r($value);
 		}
     	}	
 ?>
