c615b42ac87e3613c634d02df88b946e33b2ea17
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 16:53:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 
-<!DOCTYPE html>
 <html>
     <head>
         <title>pagina de requisição GET e POST</title>
     </head>
     <body>
-        // Campos a serem preenchidos
+        Campos a serem preenchidos
         <form method="post">
             <input type="text" name="nomeAutor" placeholder="Nome autor">
             <br>
@@ -26,16 +25,20 @@
             <input type="text" name="assuntos" placeholder="Assuntos abordados no livro">
             <br>
             <input type="submit" value="Inserir Livro">
-        </form>
- <form method="post">
-            Preencher para mostrar <br>
-            Livro específico:(código) <br>
-            <input type="text" name="LivroEspecifico"><br><br>
+    </form>
+    <form method="post">
+            Preencher para mostrar 
+            <br>
+            Livro específico:(código) 
+            <br>
+            <input type="text" name="LivroEspecifico">
+            <br><br>
             <input type="submit" name="mostrar" value="Mostrar Livros">
-         <\form>
-         <br>
-         <form method="post">
-            <input type="submit" name="limpar" value="limpar cache">
-            <\form><br>
+     <\form>
+     <br>
+     <form method="post">
+        <input type="submit" name="limpar" value="limpar cache">
+     <\form>
+     <br>
     </body>
 </html>
