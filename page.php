98e87d387c235d48f28b23f00730eddea12d4bca
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 17:47:51 -0300

    Update page.php

--- page.php
+++ page.php
@@ -1,10 +1,12 @@
 <?php
 	$dadosJson = file_get_contents('db.json');
 	$dadosJsonDecodificados = json_decode($dadosJson);
-	
-	session_start();
 
-	if($_POST['LivroEspecifico'] == '') {
-		print_r($dadosJsonDecodificados);
+	print_r($dadosJsonDecodificados);
+
+	if($_POST) {
+		
 	}
 ?>
+
+
