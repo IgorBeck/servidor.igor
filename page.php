5f50f630078bbf0d8f650d3a91e76b30a81153c3
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 18:15:15 -0300

    Update page.php

--- page.php
+++ page.php
@@ -1,9 +1,10 @@
 <?php
 	$dadosJson = file_get_contents('db.json');
 	$dadosJsonDecodificados = json_decode($dadosJson);
-	echo "teste";
 	if(isset($_POST['inserir'])) {
 		echo "Livro enviado";
+		$values = json_encode($_POST);
+		file_put_contents('db.json', $values, FILE_APPEND);
 	}
 	if(isset($_POST['MostrarTudo'])) {
 		print_r($dadosJson);	
