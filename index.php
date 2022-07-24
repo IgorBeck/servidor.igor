83f0cc72e5f9252195d720e8b75bfe70141e0ce2
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 16:49:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,13 +12,13 @@
 //Mostrar os livros
    if (isset($_POST['mostrar']) && $_POST['LivroEspecifico'] == '')){
 
-      #header( #ADICIONAR CAMINHO PARA O MOSTRAR ESPECÍFICO);
+      //header( #ADICIONAR CAMINHO PARA O MOSTRAR ESPECÍFICO);
 
    }
 //Mostrar um livro específico
    if (isset($_POST['mostrar']) && $_POST['LivroEspecifico'] !== '')){
       $_SESSION['LivroEspecifico'] = $_POST['LivroEspecifico'];
-      #header( #ADICIONAR CAMINHO PARA O MOSTRAR ESPECÍFICO);
+      //header( #ADICIONAR CAMINHO PARA O MOSTRAR ESPECÍFICO);
    }
 ?>
 <!DOCTYPE html>
