98bf9ce02d6622c029f60ce71f44f2f9212dd1d0
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 19:00:13 -0300

    Update page.php

--- page.php
+++ page.php
@@ -1,14 +1,25 @@
 <?php
 	$dadosJson = file_get_contents('db.json');
 	$dadosJsonDecodificados = json_decode($dadosJson);
+
 	if(isset($_POST['inserir'])) {
 		echo "Livro enviado";
 		$values = json_encode($_POST);
 		file_put_contents('db.json', $values, FILE_APPEND);
 	}
+
 	if(isset($_POST['MostrarTudo'])) {
 		print_r($dadosJson);	
 	}
+	if(isset($_POST['PesquisarLivro'])) {
+		print_r($dadosJson);	
+	}
+
+	<a href='https://trab3.herokuapp.com/'>
+            <br>
+            Voltar
+        </a>
+
 ?>
 
 
