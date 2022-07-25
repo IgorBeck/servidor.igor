b0e5efe16b03e0d09f777ff90ef75c377fcdd03b
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-24 21:29:53 -0300

    Update page.php

--- page.php
+++ page.php
@@ -16,7 +16,7 @@
 	}
 	
 	if(isset($_POST['PesquisarLivro'])) {
-		$isbn = $_POST['isbn'];
+		$isbn = $_POST['Livro->isbn'];
 		foreach ($dadosJsonDecodificados as $key => $value) {
     			if($value == $isbn) {
         			$msg = 'Encontrado na lista!';	
