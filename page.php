2a3ee9e31d268a5a7a600046021a458dcb2781a2
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-31 19:44:17 -0300

    Update page.php

--- page.php
+++ page.php
@@ -2,8 +2,6 @@
 	$dadosJson = file_get_contents('db.json');
 	$dadosJsonDecodificados = json_decode($dadosJson, true);
 	
-	//----------------------------------------------------------------------//
-	// enviando dados em Json pelo site
 	if(isset($_POST['inserir'])) {
 		echo "Livro enviado";
 		$values = $_POST;
@@ -11,31 +9,28 @@
 		$fp = fopen('db.json', 'w');
 		fwrite($fp, json_encode($dadosJsonDecodificados));
 		fclose($fp);
-	}
-
-	if(isset($_POST['PesquisarLivro'])) {
-		$isbn = $_POST['isbn'];
-		foreach ($dadosJsonDecodificados["Livro"] as $key => $value) {
-			foreach($value as $chave => $valor) {
-    				if($valor == $isbn) {
-					$retorna = json_encode($value); 
-					print_r($retorna);
-				}
-			}		
+	} else {
+		if($_POST) {
+			echo 'Livro enviado ao servidor';
+			$post = $_POST;
+			$dadosJsonDecodificados["Livro"][] = $post;
+			$fp = fopen('db.json', 'w');
+			fwrite($fp, json_encode($dadosJsonDecodificados));
+			fclose($fp);
 		}
-    	}
-
-	//----------------------------------------------------------------------//
-	// enviando dados em Json pelo prompt
-
-	if($_POST) {
-		echo 'Livro enviado ao servidor';
-		$post = $_POST;
-		$dadosJsonDecodificados["Livro"][] = $post;
-		$fp = fopen('db.json', 'w');
-		fwrite($fp, json_encode($dadosJsonDecodificados));
-		fclose($fp);
-	}	
+	} else {
+		if(isset($_POST['PesquisarLivro'])) {
+			$isbn = $_POST['isbn'];
+			foreach ($dadosJsonDecodificados["Livro"] as $key => $value) {
+				foreach($value as $chave => $valor) {
+    					if($valor == $isbn) {
+						$retorna = json_encode($value); 
+						print_r($retorna);
+					}
+				}		
+			}
+    		}	
+	}
 
 	if(isset($_GET)) {
 		$isbm = $_GET['isbm'];
