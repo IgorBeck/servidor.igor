481e76ef29dfa5155d02626fa93fe26d3ea2d022
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-31 19:48:10 -0300

    Update page.php

--- page.php
+++ page.php
@@ -2,25 +2,17 @@
 	$dadosJson = file_get_contents('db.json');
 	$dadosJsonDecodificados = json_decode($dadosJson, true);
 	
-	if(isset($_POST['inserir'])) {
-		echo "Livro enviado";
-		$values = $_POST;
-		$dadosJsonDecodificados["Livro"][] = $values;
-		$fp = fopen('db.json', 'w');
-		fwrite($fp, json_encode($dadosJsonDecodificados));
-		fclose($fp);
-	} else {
-		if(isset($_POST['PesquisarLivro'])) {
-			$isbn = $_POST['isbn'];
-			foreach ($dadosJsonDecodificados["Livro"] as $key => $value) {
-				foreach($value as $chave => $valor) {
-    					if($valor == $isbn) {
-						$retorna = json_encode($value); 
-						print_r($retorna);
-					}
-				}		
-			}
-    		}
+
+	if(isset($_POST['PesquisarLivro'])) {
+		$isbn = $_POST['isbn'];
+		foreach ($dadosJsonDecodificados["Livro"] as $key => $value) {
+			foreach($value as $chave => $valor) {
+    				if($valor == $isbn) {
+					$retorna = json_encode($value); 
+					print_r($retorna);
+				}
+			}		
+		}
 	} else {	
 		if($_POST) {
 			echo 'Livro enviado ao servidor';
