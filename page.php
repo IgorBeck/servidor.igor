0493aff5acd2dc9119b78923b058ea0132647886
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-31 20:15:45 -0300

    Update page.php

--- page.php
+++ page.php
@@ -3,25 +3,15 @@
 	$dadosJsonDecodificados = json_decode($dadosJson, true);
 	
 
-	if(isset($_POST['PesquisarLivro'])) {
-		$isbn = $_POST['isbn'];
-		foreach ($dadosJsonDecodificados["Livro"] as $key => $value) {
-			foreach($value as $chave => $valor) {
-    				if($valor == $isbn) {
-					$retorna = json_encode($value); 
-					print_r($retorna);
-				}
-			}		
-		}
-	} else {	
-		if($_POST) {
-			echo 'Livro enviado ao servidor';
-			$post = $_POST;
-			$dadosJsonDecodificados["Livro"][] = $post;
-			$fp = fopen('db.json', 'w');
-			fwrite($fp, json_encode($dadosJsonDecodificados));
-			fclose($fp);
-		}
+		
+	if($_POST) {
+		echo 'Livro enviado ao servidor';
+		$post = $_POST;
+		$dadosJsonDecodificados["Livro"][] = $post;
+		$fp = fopen('db.json', 'w');
+		fwrite($fp, json_encode($dadosJsonDecodificados));
+		fclose($fp);
+	
 	}
 
 	if(isset($_GET)) {
