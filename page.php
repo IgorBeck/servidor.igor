c55ce9449820d17809b04de2cf86ab0f0cab490c
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-08-01 08:56:42 -0300

    Update page.php

--- page.php
+++ page.php
@@ -5,7 +5,7 @@
 
 	if($method === 'POST') {
 		echo "Livro enviado";
-		$values = file_get_contents('php://input.json');
+		$values = file_get_contents('php://input');
 		print_r($values);
 		$dadosJsonDecodificados["Livro"][] = $values;
 		$fp = fopen('db.json', 'w');
