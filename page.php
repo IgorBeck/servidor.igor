cf6b6e2456ef4c8492d226bac5329f70ae730f90
IgorBeck <104527339+IgorBeck@users.noreply.github.com>
2022-07-31 21:33:48 -0300

    Update page.php

--- page.php
+++ page.php
@@ -5,6 +5,7 @@
 	if(isset($_POST)) {
 		echo "Livro enviado";
 		$values = $_POST;
+		print_r($values);
 		$dadosJsonDecodificados["Livro"][] = $values;
 		$fp = fopen('db.json', 'w');
 		fwrite($fp, json_encode($dadosJsonDecodificados));
